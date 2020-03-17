b4e8ed5993e9b1ede1e8107b3bfebed96cd7d96e
AnaKas-hub <61766676+AnaKas-hub@users.noreply.github.com>
2020-03-16 20:46:19 -0600

    Add files via upload

---
+++ T4_SerieFibonanci/serie.js
@@ -0,0 +1,18 @@
+function serieFibo(){
+
+    var vi1 = parseInt(document.getElementById('vi1').Value);
+    var vi2 = parseInt(document.getElementById('vi2').Value);
+    var ct = parseInt(document.getElementById('ct').Value);
+    var auxiliar = 0;
+    var conc ="";
+    conc = vi1 + " "+ vi2;
+
+    for(i = 0; i<ct; i++)
+    {
+        auxiliar = vi1+vi2;
+        vi1 = vi2;
+        vi2=auxiliar;
+        conc = conc +" "+vi2;
+    }
+    alert(conc)
+}
